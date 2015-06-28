c10d2f4663601b2b2ecee755e874e867721d2231
jinwonchoi <jinnonsbox@gmail.com>
2015-06-28 18:45:12 +0900

    bitcoin customized

---
+++ src/gitclean.sh
@@ -0,0 +1,6 @@
+find ./ -name "*.a" | xargs rm -f 
+find ./ -name "*.o" | xargs rm -f 
+rm -f bitcoin-cli bitcoind bitcoin-tx
+rm -f qt/bitcoin-qt
+rm -f test/test_bitcoin
+rm -f qt/test/test_bitcoin-qt
